96099b50f1675ff5dca6412dc2c9f8b0ed66b86d
Ivanovic14 <ival06001@utb.vaxjo.se>
2024-02-28 12:30:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uppg_6_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal.");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in en till.");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in sista talet.");
+            int tal3 = int.Parse(Console.ReadLine());
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int summa = tal1 + tal2 + tal3;
+            return summa;
+        }
+    }
+}
